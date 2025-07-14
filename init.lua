bee52539997157237d97961f963eeee323e7ee7f
Dixit <drdixit6@gmail.com>
2025-07-14 19:18:41 +0530

    in progress

--- init.lua
+++ init.lua
@@ -1,12 +1,11 @@
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_python3_provider = 0
+vim.g.loaded_node_provider = 0
 
 vim.opt.termguicolors = true
 vim.g.have_nerd_font = true
 vim.opt.background = "dark"
-vim.cmd("colorscheme retrobox")
-vim.opt.winborder = "solid"
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
@@ -20,7 +19,6 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes:2"
--- vim.opt.colorcolumn = "80"
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.splitkeep = "screen"
@@ -56,32 +54,9 @@ vim.opt.filetype.indent = "off"  -- Lua way (if supported)
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
--- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
--- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
--- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
--- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
--- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
--- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
--- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
-
--- keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
--- keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
-
--- keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
--- keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
--- keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
--- keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
-
--- keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
--- keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
--- keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
--- keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
--- keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.hl.on_yank()
   end,
@@ -115,12 +90,12 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
-    --------------------
-    --- PLUGIN START ---
-    --------------------
     {
       "nvim-treesitter/nvim-treesitter-textobjects",
       dependencies = { "nvim-treesitter/nvim-treesitter" },
+      init = function()
+        require("nvim-treesitter.install").compilers = { "zig" }
+      end,
       config = function()
         require("nvim-treesitter.configs").setup({
           ensure_installed = { "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc" },
@@ -167,45 +142,194 @@ require("lazy").setup({
         })
       end
     },
-    ------------------
-    --- PLUGIN END ---
-    ------------------
-  },
-  install = { colorscheme = { "retrobox" } },
-  checker = { enabled = true },
-  ui = {
-    size = { width = 0.85, height = 0.75 },
-    border = "solid",
-    backdrop = 10,
-  },
-})
+    {
+      "folke/tokyonight.nvim",
+      priority = 1000, -- make sure to load this before all the other start plugins
+      config = function()
+        local bg = "#011628"
+        local bg_dark = "#011423"
+        local bg_highlight = "#143652"
+        local bg_search = "#0A64AC"
+        local bg_visual = "#275378"
+        local fg = "#CBE0F0"
+        local fg_dark = "#B4D0E9"
+        local fg_gutter = "#627E97"
+        local border = "#547998"
 
+        require("tokyonight").setup({
+          style = "night",
+          on_colors = function(colors)
+            colors.bg = bg
+            colors.bg_dark = bg_dark
+            colors.bg_float = bg_dark
+            colors.bg_highlight = bg_highlight
+            colors.bg_popup = bg_dark
+            colors.bg_search = bg_search
+            colors.bg_sidebar = bg_dark
+            colors.bg_statusline = bg_dark
+            colors.bg_visual = bg_visual
+            colors.border = border
+            colors.fg = fg
+            colors.fg_dark = fg_dark
+            colors.fg_float = fg
+            colors.fg_gutter = fg_gutter
+            colors.fg_sidebar = fg_dark
+          end,
+        })
+        -- load the colorscheme here
+        vim.cmd([[colorscheme tokyonight]])
+      end,
+    },
+    {
+      "nvim-lualine/lualine.nvim",
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      config = function()
+        local lualine = require("lualine")
+        local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 
-vim.lsp.enable({
-    "lua_ls"
+        local colors = {
+          blue = "#65D1FF",
+          green = "#3EFFDC",
+          violet = "#FF61EF",
+          yellow = "#FFDA7B",
+          red = "#FF4A4A",
+          fg = "#c3ccdc",
+          bg = "#112638",
+          inactive_bg = "#2c3043",
+        }
+
+        local my_lualine_theme = {
+          normal = {
+            a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          insert = {
+            a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          visual = {
+            a = { bg = colors.violet, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          command = {
+            a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          replace = {
+            a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+            b = { bg = colors.bg, fg = colors.fg },
+            c = { bg = colors.bg, fg = colors.fg },
+          },
+          inactive = {
+            a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
+            b = { bg = colors.inactive_bg, fg = colors.semilightgray },
+            c = { bg = colors.inactive_bg, fg = colors.semilightgray },
+          },
+        }
+
+        -- configure lualine with modified theme
+        lualine.setup({
+          options = {
+            theme = my_lualine_theme,
+          },
+          sections = {
+            lualine_x = {
+              {
+                lazy_status.updates,
+                cond = lazy_status.has_updates,
+                color = { fg = "#ff9e64" },
+              },
+              { "encoding" },
+              { "fileformat" },
+              { "filetype" },
+            },
+          },
+        })
+      end,
+    },
+    {
+      "ibhagwan/fzf-lua",
+      -- optional for icon support
+      dependencies = { "nvim-tree/nvim-web-devicons" },
+      -- or if using mini.icons/mini.nvim
+      -- dependencies = { "echasnovski/mini.icons" },
+      opts = {},
+      config = function()
+        require("fzf-lua").setup({
+          fzf_colors = {
+            true,
+            ["fg"]          = { "fg", "CursorLine" },
+            ["bg"]          = { "bg", "Normal" },
+            ["hl"]          = { "fg", "Comment" },
+            ["fg+"]         = { "fg", "Normal", "underline" },
+            ["bg+"]         = { "bg", { "CursorLine", "Normal" } },
+            ["hl+"]         = { "fg", "Statement" },
+            ["info"]        = { "fg", "PreProc" },
+            ["prompt"]      = { "fg", "Conditional" },
+            ["pointer"]     = { "fg", "Exception" },
+            ["marker"]      = { "fg", "Keyword" },
+            ["spinner"]     = { "fg", "Label" },
+            ["header"]      = { "fg", "Comment" },
+            ["gutter"]      = "-1",
+          },
+        })
+        vim.keymap.set("n", "<leader><leader>", "<cmd>FzfLua buffers<cr>", { desc = "[ ] Find existing buffers" })
+        vim.keymap.set("n", "<leader>sh", "<cmd>FzfLua help_tags<cr>", { desc = "[S]earch [H]elp" })
+        vim.keymap.set("n", "<leader>sk", "<cmd>FzfLua keymaps<cr>", { desc = "[S]earch [K]eymaps" })
+        vim.keymap.set("n", "<leader>sf", "<cmd>FzfLua files<cr>", { desc = "[S]earch [F]iles" })
+        vim.keymap.set("n", "<leader>sg", "<cmd>FzfLua live_grep<cr>", { desc = "[S]earch by [G]rep" })
+        vim.keymap.set("n", "<leader>sw", "<cmd>FzfLua grep_cword<cr>", { desc = "[S]earch current [W]ord" })
+      end,
+    },
+    { -- Adds git related signs to the gutter, as well as utilities for managing changes
+      "lewis6991/gitsigns.nvim",
+      opts = {
+        signs = {
+          add = { text = '+' },
+          change = { text = '~' },
+          delete = { text = '_' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
+        },
+      },
+    },
+
+    {
+      "github/copilot.vim",
+    },
+
+  },
+  install = { colorscheme = { "tokyonight" } },
+  checker = {
+    enabled = true,
+    notify = false,
+  },
 })
 
 vim.diagnostic.config({
-    -- virtual_lines = true,
-    virtual_text = true,
-    underline = true,
-    update_in_insert = false,
-    severity_sort = true,
-    float = {
-        border = "rounded",
-        source = true,
+  -- virtual_lines = true,
+  virtual_text = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  float = {
+    border = "rounded",
+    source = true,
+  },
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "󰅚 ",
+      [vim.diagnostic.severity.WARN] = "󰀪 ",
+      [vim.diagnostic.severity.INFO] = "󰋽 ",
+      [vim.diagnostic.severity.HINT] = "󰌶 ",
     },
-    signs = {
-        text = {
-            [vim.diagnostic.severity.ERROR] = "󰅚 ",
-            [vim.diagnostic.severity.WARN] = "󰀪 ",
-            [vim.diagnostic.severity.INFO] = "󰋽 ",
-            [vim.diagnostic.severity.HINT] = "󰌶 ",
-        },
-        numhl = {
-            [vim.diagnostic.severity.ERROR] = "ErrorMsg",
-            [vim.diagnostic.severity.WARN] = "WarningMsg",
-        },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+      [vim.diagnostic.severity.WARN] = "WarningMsg",
     },
+  },
 })
-
--- lsp/lua_ls.lua
+++
@@ -1,7 +0,0 @@
-return {
-  cmd = { "lua-language-server" },
-  filetypes = { "lua" },
-  root_markers = { ".git", ".luarc.json", ".luarc.jsonc" },
-  single_file_support = true,
-  log_level = vim.lsp.protocol.MessageType.Warning,
-}

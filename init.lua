5eb622e1623c52c0e6d65a08a1d0519a1d40f72c
Dixit <drdixit6@gmail.com>
2025-10-29 19:49:19 +0530

    💤 LazyVim Init Commit 803bc18

--- init.lua
+++ init.lua
@@ -1,19 +1,2 @@
-vim.g.loaded_perl_provider = 0 -- disable perl support
-vim.g.loaded_ruby_provider = 0 -- disable ruby support
-vim.g.loaded_python3_provider = 0 -- disable python3 support
-
--- Neovide specific settings
--- if vim.g.neovide then
---   vim.g.neovide_opacity = 0.90
---   vim.g.neovide_refresh_rate = 60
---   vim.g.neovide_background_color = "#011628"
---   vim.g.neovide_floating_blur_amount_x = 12.0
---   vim.g.neovide_floating_blur_amount_y = 12.0
---   vim.g.neovide_window_blurred = true
---   vim.o.guifont = "JetBrainsMono Nerd Font:h13"
--- end
-
-require("config.options")
-require("config.keymaps")
+-- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- require("config.wezterm")

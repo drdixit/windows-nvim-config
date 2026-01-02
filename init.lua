25463202715ff66c34b5d8adb2b3a0b3765c186c
Dixit <drdixit6@gmail.com>
2026-01-02 14:51:27 +0530

    Added: Treesitter and Telescope

--- init.lua
+++ init.lua
@@ -2,12 +2,17 @@ require('options')
 
 -- require('colorscheme').setup('ayu-dark')
 -- require('colorscheme').setup('material-darker')
+-- require('colorscheme').setup('material')
 require('colorscheme').setup('default-dark')
-
 vim.cmd([[
   highlight Normal guibg=none
   highlight NormalFloat guibg=none
   highlight SignColumn guibg=none
   highlight LineNr guibg=none
 ]])
+
+vim.pack.add({ "https://github.com/nvim-tree/nvim-web-devicons.git" })
+require('plugins.treesitter')
+require('plugins.telescope')
+require('plugins.gitsigns')
 
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,77 @@
+vim.pack.add({
+  { src="https://github.com/nvim-treesitter/nvim-treesitter", version="master" },
+})
+
+vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" })
+
+require("nvim-treesitter.configs").setup({
+  ensure_installed = { "lua", "luadoc", "dart", },
+  auto_install = true,
+  highlight = { enable = true },
+  indent = { enable = true },
+
+  incremental_selection = {
+
+    enable = true,
+    keymaps = {
+
+      init_selection = "<C-space>",
+      node_incremental = "<C-space>",
+      scope_incremental = false,
+      node_decremental = "<bs>",
+    },
+
+  },
+
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+        ["as"] = {
+          query = "@local.scope",
+          query_group = "locals",
+
+          desc = "Select language scope",
+        },
+      },
+      selection_modes = {
+        ["@parameter.outer"] = "v", -- charwise
+        ["@function.outer"] = "V", -- linewise
+        ["@class.outer"] = "<c-v>", -- blockwise
+      },
+      include_surrounding_whitespace = true,
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>A"] = "@parameter.inner",
+      },
+    },
+  },
+})
+
+vim.api.nvim_create_autocmd('PackChanged', {
+  desc = 'Handle nvim-treesitter updates',
+  group = vim.api.nvim_create_augroup('nvim-treesitter-pack-changed-update-handler', { clear = true }),
+  callback = function(event)
+    if event.data.kind == 'update' and event.data.spec.name == 'nvim-treesitter' then
+      vim.notify('nvim-treesitter updated, running TSUpdate...', vim.log.levels.INFO)
+      ---@diagnostic disable-next-line: param-type-mismatch
+      local ok = pcall(vim.cmd, 'TSUpdate')
+      if ok then
+        vim.notify('TSUpdate completed successfully!', vim.log.levels.INFO)
+      else
+        vim.notify('TSUpdate command not available yet, skipping', vim.log.levels.WARN)
+      end
+    end
+  end,
+})

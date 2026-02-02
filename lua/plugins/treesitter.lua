57db49ad6955adf39b8f06fd018149dd646deb7f
Dixit <drdixit6@gmail.com>
2026-02-02 10:04:01 +0530

    Update: Treesitter changed some stuff

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,20 +10,29 @@ return {
     --
     -- so if you use master branch then you get one combined config for both treesitter and text objects
     -- but if you use main branch then you need to seperately configure text objects through ("nvim-treesitter-textobjects").setup
+    --
+    --
+    -- incremental_selection is also gone keep that in mind
+    -- incremental_selection = {
+    --   enable = true,
+    --   keymaps = {
+    --     init_selection = "<C-Space>",
+    --     node_incremental = "<C-Space>",
+    --     scope_incremental = false,
+    --     node_decremental = "<BS>",
+    --   },
+    -- },
+    -- you can use this plugins for incremental selection daliusd/incr.nvim
+    -- or if you relay on any modules that is from master branch checkout this plugin MeanderingProgrammer/treesitter-modules.nvim
+    --
+    -- also they drop some commands as well that you run in command line
+    -- :TSInstallInfo :TSToggle :TSEnable :TSDisable :TSBufEnable etc.
+    --
     require("nvim-treesitter.config").setup({
       ensure_installed = { "php", "bash", "c", "diff", "html", "lua", "luadoc", "markdown", "markdown_inline", "query", "vim", "vimdoc", "dart", },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
     })
   end,
 }
